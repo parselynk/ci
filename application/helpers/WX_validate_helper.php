b4b0e538f775284f4db12b674c039e2af519bc5e
rayxa <rayxa@Rezas-MacBook-Pro.local>
2016-07-24 02:24:20 +0800

    some bugs were checked for 'validate_parameter_array'

--- application/helpers/WX_validate_helper.php
+++ application/helpers/WX_validate_helper.php
@@ -6,11 +6,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 function validate($field='',$param=''){
   
    $trace_message = get_calling_function();
+   //var_dump($trace_message);die;
    $db_fields = isset($trace_message['object'])?$trace_message['object']::$db_fields:array();
    
    if(isset($field) && $field!='') {
        if(is_array($field)){
-           validate_field_parameter_array($field,$db_fields,$trace_message['object']);
+           validate_field_parameter_array($field,$db_fields,$trace_message);
            return true;
        }
        
@@ -42,7 +43,8 @@ function is_valid($field, $db_fields,$trace_message){
     if(in_array($field, $db_fields)){
         return true;
     }else{
-        $message = ($field!='')? "$field is not found in \$db_fields": "EMPTY Filed is passed to query";
+        $object = get_class($trace_message['object']);
+        $message = ($field!='')? "<strong>\"$field\"</strong> is not found in <strong>{$object}</strong> \$db_fields": "EMPTY Filed is passed to query";
         throw new Exception("Model Validation: $message ". $trace_message['message'],406); 
     }
 }
@@ -61,7 +63,7 @@ function validate_parameter_array($param_array,$trace_message ){
 function validate_field_parameter_array($field_parameter_array,$allowed_db_fields,$trace_message ){
     foreach ($field_parameter_array as $field=>$parameter) {
         if(is_valid($field, $allowed_db_fields,$trace_message))  {    // $values cannot be empty for parameters
-            if(!isset($value) || $value ==''){
+            if(!isset($parameter) || $parameter ==''){
                 throw new Exception("Model Validation: Empty Parameter(s) found in <strong>parameter array</strong> ". $trace_message['message']);
             }
         }
