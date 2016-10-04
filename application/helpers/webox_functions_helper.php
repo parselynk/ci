5128517ef9fbc0537452744b6f7ab826716664c1
Reza Kay <reza.karkehabadi@gmail.com>
2016-10-04 22:24:51 +0800

    Comments removed
    some comments been removed

--- application/helpers/webox_functions_helper.php
+++ application/helpers/webox_functions_helper.php
@@ -1,14 +1,5 @@
 <?php
 
-//function exception_error_handler($severity, $message, $file, $line) {
-//    if (!(error_reporting() & $severity)) {
-//        // This error code is not included in error_reporting
-//        return;
-//    }
-//    throw new ErrorException($message, 0, $severity, $file, $line);
-//}
-
-
 function error_message($e, $last_query="") {
     if ($e) {
         $output = array();
